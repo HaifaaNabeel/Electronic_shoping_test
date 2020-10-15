6e8b09df0aa1717de7b136ad42a1021e66189e91
suad alwajeeh <sd1.alwajeeh1@gmail.com>
2020-10-15 13:24:13 +0300

    header and footer with home page

---
+++ start/view/footer.php
@@ -0,0 +1,58 @@
+<!--================ Start Footer Area =================-->
+
+	<footer class="footer-area section-gap">
+		<div class="container ">
+			<div class="row">
+				<div class="col-lg-12" style="">
+					<div class="text-center">
+						<h4 style="text-center">
+متجر الكتروني لبيع المنتجات الالكترونيه
+                            </br>
+                        </h4>
+                    </div>
+				</div>
+            <div class="col-md-4 col-sm-12 " style="">
+					<div class="text-center">
+                       
+						<ul>
+                         <h6>روابط عامة</h6>    
+                            <li>من نحن</li>
+                            <li>العروض </li>
+                            <li> تواصل معنا</li>                            
+                        </ul>
+                    </div>
+				</div>
+            <div class="col-md-4 col-sm-12" style="">
+					<div class="text-center">
+                        
+						<ul>
+                            <h6> الاصناف</h6>
+                            <li>موبايلات</li>
+                            <li>لابتوبات </li>
+                            <li>  تابليتات</li>                            
+                        </ul>
+                    </div>
+				</div>
+            <div class="col-md-4 col-sm-12" style="">
+					<div class="text-center">
+						<ul>
+                       <h6>للتواصل </h6>
+                            <li><span class="ion-social-facebook-outline"></span>:www.facebook.com </li>
+                            <li><span class="ion-social-whatsapp-outline"></span>:www.whatsapp.com </li>
+                            <li><span class="ion-social-twitter-outline"></span>:www.twitter.com</li>                            
+                        </ul>
+                    </div>
+				</div>
+            <div class="col-lg-12 foo" style="">
+					<div class="text-center">
+						<p style="text-center">
+                            @2020حقوق الطبع محفوظة لفريق <span style="color:#fd519a;">webDev</span>          
+                        </p>
+                    </div>
+				</div>
+			</div>
+        </div>
+
+	</footer>
+</body>
+</html>
---
+++ start/view/index.php
@@ -0,0 +1,241 @@
+<?php include'header.php'; ?>
+      <slider style="top: -56px;">
+  <div class="layer1"></div>   
+<div id="demo" class="carousel slide" data-ride="carousel">
+ <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="3"></li>
+  </ul>
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../assets/img/accessories_galaxy-note20_s.jpg" alt="Los Angeles" >
+        <div class="carousel-caption animated fadeInLeft " style="animation-delay:2s;">
+    <h3>Los Angeles</h3>
+    <p>We had such a great time in LA!</p>
+  </div>
+    </div>
+    
+    <!-- /.Third slide -->
+    <div class="carousel-item">
+      <img src="../assets/img/banner_galaxy-a8.png" alt="Chicago" >
+           <div class="carousel-caption animated fadeInLeft " style="animation-delay:2s;">
+    <h3>Los Angeles</h3>
+    <p>We had such a great time in LA!</p>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/img/banner_galaxy-note8.png" alt="New York">
+        <div class="carousel-caption animated zoomIn " style="animation-delay:2s;">
+    <h3>Los Angeles</h3>
+    <p>We had such a great time in LA!</p>
+  </div>
+    </div>
+      
+  </div>
+  
+</div>
+    </slider>   
+ </br></br>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h3 class="text-center mb-3 mt-3">ساعات</h3>
+            </div>
+             <div class="col-sm-12">
+              <div class="img-gallery  owl-carousel owl-theme">
+              <div class=" text-center mr-1 ml-1  mb-2">
+                  <div class="card">
+                 <div style="height:auto ;" class="table-block ">
+                    <img alt=""  src="../assets/img/banner_gear-sport.png" style="height: 272px;" />
+                 </div>
+                <div class="card-body">
+                    <h6>ساعه رياضيه ذكية</h6>
+                    <h6>8000RY</h6>
+                    <h6>
+                        <form action="" method="post">
+                        <button class="btn product_btn m-1" type="submit" name="card"><span class="ion-ios-cart-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="favorite"><span class="ion-android-favorite-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="filter"><span class="ion-ios-color-filter-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="more"><span class="ion-ios-more-outline"></span></button>
+                        </form>
+                    </h6>                    
+                </div>
+              </div> 
+            </div>
+              <div class=" text-center mr-1 ml-1  mb-2">
+                  <div class="card">
+                 <div style="height:auto ;" class="table-block ">
+                    <img alt=""  src="../assets/img/banner_gear-360.png" style="height: 272px;" />
+                 </div>
+                <div class="card-body">
+                    <h6>ساعه رياضيه ذكية</h6>
+                    <h6>8000RY</h6>
+                    <h6>
+                        <form action="" method="post">
+                        <button class="btn product_btn m-1" type="submit" name="card"><span class="ion-ios-cart-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="favorite"><span class="ion-android-favorite-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="filter"><span class="ion-ios-color-filter-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="more"><span class="ion-ios-more-outline"></span></button>
+                        </form>
+                    </h6>                    
+                </div>
+              </div> 
+            </div>
+              <div class=" text-center mr-1 ml-1  mb-2">
+                  <div class="card">
+                 <div style="height:auto ;" class="table-block ">
+                    <img alt=""  src="../assets/img/banner_gear-s3.png" style="height: 272px;" />
+                 </div>
+                <div class="card-body">
+                    <h6>ساعه رياضيه ذكية</h6>
+                    <h6>8000RY</h6>
+                    <h6>
+                        <form action="" method="post">
+                        <button class="btn product_btn m-1" type="submit" name="card"><span class="ion-ios-cart-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="favorite"><span class="ion-android-favorite-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="filter"><span class="ion-ios-color-filter-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="more"><span class="ion-ios-more-outline"></span></button>
+                        </form>
+                    </h6>                    
+                </div>
+              </div> 
+            </div>      
+             </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h3 class="text-center mb-3 mt-3">موبايلات</h3>
+            </div>
+             <div class="col-sm-12">
+              <div class="img-gallery  owl-carousel owl-theme">
+              <div class=" text-center mr-1 ml-1  mb-2">
+                  <div class="card">
+                 <div style="height:auto ;" class="table-block ">
+                    <img alt=""  src="../assets/img/accessories_galaxy-note20_s.jpg" style="height: 272px;" />
+                 </div>
+                <div class="card-body">
+                    <h6>ساعه رياضيه ذكية</h6>
+                    <h6>8000RY</h6>
+                    <h6>
+                        <form action="" method="post">
+                        <button class="btn product_btn m-1" type="submit" name="card"><span class="ion-ios-cart-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="favorite"><span class="ion-android-favorite-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="filter"><span class="ion-ios-color-filter-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="more"><span class="ion-ios-more-outline"></span></button>
+                        </form>
+                    </h6>                    
+                </div>
+              </div> 
+            </div>
+              <div class=" text-center mr-1 ml-1  mb-2">
+                  <div class="card">
+                 <div style="height:auto ;" class="table-block ">
+                    <img alt=""  src="../assets/img/banner_galaxy-a8.png" style="height: 272px;" />
+                 </div>
+                <div class="card-body">
+                    <h6>ساعه رياضيه ذكية</h6>
+                    <h6>8000RY</h6>
+                    <h6>
+                        <form action="" method="post">
+                        <button class="btn product_btn m-1" type="submit" name="card"><span class="ion-ios-cart-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="favorite"><span class="ion-android-favorite-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="filter"><span class="ion-ios-color-filter-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="more"><span class="ion-ios-more-outline"></span></button>
+                        </form>
+                    </h6>                    
+                </div>
+              </div> 
+            </div>
+              <div class=" text-center mr-1 ml-1  mb-2">
+                  <div class="card">
+                 <div style="height:auto ;" class="table-block ">
+                    <img alt=""  src="../assets/img/banner_galaxy-s9.png" style="height: 272px;" />
+                 </div>
+                <div class="card-body">
+                    <h6>ساعه رياضيه ذكية</h6>
+                    <h6>8000RY</h6>
+                    <h6>
+                        <form action="" method="post">
+                        <button class="btn product_btn m-1" type="submit" name="card"><span class="ion-ios-cart-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="favorite"><span class="ion-android-favorite-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="filter"><span class="ion-ios-color-filter-outline"></span></button>
+                        <button class="btn product_btn m-1" type="submit" name="more"><span class="ion-ios-more-outline"></span></button>
+                        </form>
+                    </h6>                    
+                </div>
+              </div> 
+            </div>      
+             </div>
+            </div>
+        </div>
+    </div>
+</section> 
+ </br></br>
+
+<section class="points">
+<h1 class="">   علامات تجاريه</h1>
+<svg id="mesvg" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1400 197.72"><path class="cls-1" d="M0,262.71S322.94-46.71,686.47,108.59,1182.94,245.06,1400,118V262.71Z" transform="translate(0 -64.98)"/></svg>
+</section>
+<section class="points2">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-2">
+                <div class="">
+                <img class="img-fluid" src="../assets/img/Download-Lenovo-Logo-PNG-Transparent-Image-420x186.png">
+                </div>
+            </div>
+             <div class="col-sm-12 col-md-2">
+                <div class="">
+                <img class="img-fluid" src="../assets/img/47050_preview.png">
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-2">
+                <div class="">
+                <img class="img-fluid" src="../assets/img/177-1774997_htc-logo-transparent-background-download-htc-logo.png">
+                </div>
+            </div>
+             <div class="col-sm-12 col-md-2">
+                <div class="">
+                <img class="img-fluid" src="../assets/img/Huawei-Symbol.png">
+                </div>
+            </div>
+             <div class="col-sm-12 col-md-2">
+                <div class="">
+                <img class="img-fluid" src="../assets/img/laptop-desktop-dell-png-logo-8.png">
+                </div>
+            </div>
+             <div class="col-sm-12 col-md-2">
+                <div class="">
+                <img class="img-fluid" src="../assets/img/Sony-Logo-1961%E2%80%931962.png">
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-2">
+                <div class="">
+                <img class="img-fluid" src="../assets/img/47050_preview.png">
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-2">
+                <div class="">
+                <img class="img-fluid" src="../assets/img/177-1774997_htc-logo-transparent-background-download-htc-logo.png">
+                </div>
+            </div>
+             <div class="col-sm-12 col-md-2">
+                <div class="">
+                <img class="img-fluid" src="../assets/img/samsung_logo_PNG16.png">
+                </div>
+            </div>
+             
+        </div>
+    </div>
+</section>
+
+<?php include'footer.php'; ?>

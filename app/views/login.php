f7b895440543dd1161f9aa9fcca85a62521e2ef6
HaifaaNabeel <hauof53@gmail.com>
2020-10-19 19:50:08 +0300

    change contact and login pages

--- app/views/login.php
+++ app/views/login.php
@@ -1,16 +1,8 @@
 <?php include'header.php'; ?>
 
 <head>
-    <title>singup</title>
-    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' rel='stylesheet'>
-    <link href='https://use.fontawesome.com/releases/v5.8.1/css/all.css' rel='stylesheet'>
     <link rel="stylesheet" href="app/assets/css/loginstyle.css">
 
-
-    <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-    <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
-    <script type='text/javascript' src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'></script>
-    <script type='text/javascript'></script>
 </head>
     
 <br><br><br><br><br>
@@ -19,20 +11,20 @@
             <div class="col-md-6 col-sm-6 col-xs-6">
                 <div class="card">
                     <form class="box">
-                        <h1>Login</h1>
-                        <p class="text-muted"> Please enter your login and password!</p>
-                         <input type="text" name="" placeholder="Username" required>
-                         <input type="password" name="" placeholder="Password" required>
-                         <a class="forgot text-muted" href="#">Forgot password?</a>
-                         <input type="submit" name="" value="Login" href="#">
-                         <div class="col-md-12">
+                        <h1>تسجيل الدخول </h1>
+                        <p class="text-muted"> ادخل اسمك وكلمة المرور للتمكن من الوصول الى حسابك</p>
+                         <input type="text" name="" placeholder="اسم المستخدم " required>
+                         <input type="password" name="" placeholder="كلمة المرور " required>
+                         <a class="forgot text-muted" href="#">نسيت كلمة المرور ؟</a>
+                         <input type="submit" name="" value="الدخول " href="#">
+                         <div class="col-md-12 col-sm-12 col-xs-12">
+                         <p class="text-muted"> او لتسجيل الدخول بحسابات اخرى </p>
                             <ul class="social-network social-circle">
-                                <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="#" class="icoTwitter" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-                                <li><a href="#" class="icoGoogle" title="Google +"><i class="fab fa-google-plus"></i></a></li>
-
+                                <li><a href="#" class="icoFacebook" title="Facebook"><i class="ion-social-facebook-outline"></i></a></li>
+                                <li><a href="#" class="icoTwitter" title="Twitter"><i class="ion-social-twitter-outline"></i></a></li>
+                                <li><a href="#" class="icoGoogle" title="Google +"><i class="ion-social-google-outline"></i></a></li>
                             </ul><br>
-                        <a class="forgot text-muted" href="./singup"> OR singup now </a>
+                        <a class="forgot text-muted" href="./singup"> او لإنشاء حساب والتسجيل الان  </a>
 
                         </div>
                     </form>
--- app/views/singup.php
+++ app/views/singup.php
@@ -1,44 +1,31 @@
 <?php include'header.php'; ?>
-
-
-
     
 <head>
-    
-    <title>singup</title>
-    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' rel='stylesheet'>
-    <link href='https://use.fontawesome.com/releases/v5.8.1/css/all.css' rel='stylesheet'>
     <link rel="stylesheet" href="app/assets/css/loginstyle.css">
-
-
-    <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-    <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
-    <script type='text/javascript' src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'></script>
-    <script type='text/javascript'></script>
 </head>
     
 <br><br><br><br><br>
     <div class="container">
         <div class="row">
-            <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="col-md-6 col-sm-6 col-xs-6 ">
                 <div class="card">
-                    <form class="box">
-                        <h1>Singup</h1>
-                        <p class="text-muted"> Welcome in our shop you can regiser now ^_^</p>
-                         <input type="text" name="" placeholder="Username" required>
-                         <input type="password" name="" placeholder="Password" required>
-                         <input type="email" name="" placeholder="email" required>
+                <form class="box">
+                        <h1>انشاء حساب </h1>
+                        <p class="text-muted"> اهلا بك في متجرنا لإنشاء حساب قم بإدخال بياناتك</p>
+                         <input type="text" name="" placeholder="اسم المستخدم " required>
+                         <input type="password" name="" placeholder="كلمة المرور " required>
+                         <input type="email" name="" placeholder="بريدك الالكتروني " required>
 
                          
-                         <input type="submit" name="" value="singup" href="#">
-                         <div class="col-md-12">
-                            <ul class="social-network social-circle">
-                                <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="#" class="icoTwitter" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-                                <li><a href="#" class="icoGoogle" title="Google +"><i class="fab fa-google-plus"></i></a></li>
-
+                         <input type="submit" name="" value="التسجيل الان " href="#">
+                         <div class="col-md-12 col-sm-12 col-xs-12 ">
+                         <p class="text-muted"> او لتسجيل الدخول بحسابات اخرى </p>
+                         <ul class="social-network social-circle">
+                                <li><a href="#" class="icoFacebook" title="Facebook"><i class="ion-social-facebook-outline"></i></a></li>
+                                <li><a href="#" class="icoTwitter" title="Twitter"><i class="ion-social-twitter-outline"></i></a></li>
+                                <li><a href="#" class="icoGoogle" title="Google +"><i class="ion-social-google-outline"></i></a></li>
                             </ul><br>
-                        <a class="forgot text-muted" href="./login"> OR login now </a>
+                        <a class="forgot text-muted" href="./login"> او للدخول الى حساب موجود س </a>
 
                         </div>
                     </form>

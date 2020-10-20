1fc9504d15db5b8cc51ec7efbe3a1329068fd855
HaifaaNabeel <hauof53@gmail.com>
2020-10-20 22:16:42 +0300

    change style of login and singup pages

--- app/views/login.php
+++ app/views/login.php
@@ -1,37 +1,150 @@
 <?php include'header.php'; ?>
 
-<head>
-    <link rel="stylesheet" href="app/assets/css/loginstyle.css">
 
-</head>
     
-<br><br><br><br><br>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 col-sm-6 col-xs-6">
-                <div class="card">
-                    <form class="box">
-                        <h1>تسجيل الدخول </h1>
-                        <p class="text-muted"> ادخل اسمك وكلمة المرور للتمكن من الوصول الى حسابك</p>
-                         <input type="text" name="" placeholder="اسم المستخدم " required>
-                         <input type="password" name="" placeholder="كلمة المرور " required>
-                         <a class="forgot text-muted" href="#">نسيت كلمة المرور ؟</a>
-                         <input type="submit" name="" value="الدخول " href="#">
-                         <div class="col-md-12 col-sm-12 col-xs-12">
-                         <p class="text-muted"> او لتسجيل الدخول بحسابات اخرى </p>
-                            <ul class="social-network social-circle">
-                                <li><a href="#" class="icoFacebook" title="Facebook"><i class="ion-social-facebook-outline"></i></a></li>
-                                <li><a href="#" class="icoTwitter" title="Twitter"><i class="ion-social-twitter-outline"></i></a></li>
-                                <li><a href="#" class="icoGoogle" title="Google +"><i class="ion-social-google-outline"></i></a></li>
-                            </ul><br>
-                        <a class="forgot text-muted" href="./singup"> او لإنشاء حساب والتسجيل الان  </a>
+<body>
+    <br><br><br><br><br><br>
+<style>
+    ul.social-network {
+    list-style: none;
+    display: inline;
+    margin-left: 0 !important;
+    padding: 0
+}
 
-                        </div>
-                    </form>
-                </div>
+ul.social-network li {
+    display: inline;
+    margin: 0 5px
+}
+
+.social-network a.icoFacebook:hover {
+    background-color: #3B5998
+}
+
+.social-network a.icoTwitter:hover {
+    background-color: #33ccff
+}
+
+.social-network a.icoGoogle:hover {
+    background-color: #BD3518
+}
+.social-network a.icoGmail:hover {
+    background-color: #922a13
+}
+.social-network a.icoFacebook:hover i,
+.social-network a.icoTwitter:hover i,
+.social-network a.icoGmail:hover i,
+.social-network a.icoGoogle:hover i {
+    color: black
+}
+
+a.socialIcon:hover,
+.socialHoverClass {
+    color: #44BCDD
+}
+
+.social-circle li a {
+    display: inline-block;
+    position: relative;
+    margin: 0 auto 0 auto;
+    border-radius: 50%;
+    text-align: center;
+    width: 50px;
+    height: 50px;
+    font-size: 20px
+}
+
+.social-circle li i {
+    margin: 0;
+    line-height: 50px;
+    text-align: center
+}
+
+.social-circle li a:hover i,
+.triggeredHover {
+    transform: rotate(360deg);
+    transition: all 0.2s
+}
+
+.social-circle i {
+    color: black;
+    transition: all 0.8s;
+    transition: all 0.8s
+}
+.card button[type="submit"]{
+    border-radius: 24px;
+    cursor: pointer;
+    text-align: center;
+
+
+
+}
+
+.card input, button[type="submit"],label {
+    border: 0;
+    background: none;
+    display: block;
+    margin: 10px ;
+    outline: none;
+    color: black;
+    border-radius: 24px;
+    transition: 0.25s;
+    width: 400px;
+    height: 50px;
+}
+.card input,button[type="submit"]{    border: 2px solid #2ecc71;}
+.cadr button[type="submit"]:hover {
+    background: #2ecc71
+}
+
+</style>
+     <main>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4 "></div>
+        <div class="col-md-4 ">
+            <div class="card" style="border-radius: 24px; padding:10px" >
+                <form class="" action="" method="POST">
+                    <h2 class="text-center" style="color: #000;"> تسجيل الدخول </h2>
+                    <p class="text-center"> ادخل اسمك وكلمة المرور للتمكن من الوصول الى حسابك</p>
+                    <div class="form-group text-right">
+                        <label class="text-right" for="uname">اسم المستخدم </label>
+                        <input type="text" class="form-control text-right " id="uname" placeholder="اسم المستخدم" name="uname" required>
+                    </div>
+                    <div class="form-group text-right">
+                        <label class="text-right" for="uname">كلمة المرور  </label>
+                        <input type="password" class="form-control text-right" id="uname" placeholder="كلمة المرور " name="uname" required>
+                    </div>
+                     <a  href="#"><p class="text-muted text-center">نسيت كلمة المرور ؟ </p></a> <br>
+
+
+                     <button type="submit" class="btn  btn-primary"  id="submit">الدخول </button>
+<br><br>
+                     <p class="text-muted text-center"> او لتسجيل الدخول بحسابات اخرى </p>
+                     
+                    
+                        <ul class="social-network social-circle navbar-nav" style="text-align:center;padding-left:27%">
+                            
+                            <li><a href="#" class="icoFacebook" title="Facebook"><i class="ion-social-facebook-outline"></i></a></li>
+                            <li><a href="#" class="icoTwitter" title="Twitter"><i class="ion-social-twitter-outline"></i></a></li>
+                            <li><a href="#" class="icoGoogle" title="Google +"><i class="ion-social-google-outline"></i></a></li>
+                        </ul><br>
+                    <a href="./singup"> <p class="text-muted text-center">او لإنشاء حساب والتسجيل الان  </p></a>
+
+                    
+                </form>
+                
             </div>
         </div>
+        <div class="col-md-4 "></div>
+
     </div>
+    
+</div>
+
+
+<br><br><br><br><br><br>
+    </main>
 
 <br><br><br><br>
 <?php include'footer.php'; ?>
--- app/views/singup.php
+++ app/views/singup.php
@@ -1,33 +1,133 @@
 <?php include'header.php'; ?>
     
-<head>
-    <link rel="stylesheet" href="app/assets/css/loginstyle.css">
-</head>
-    
-<br><br><br><br><br>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 col-sm-6 col-xs-6 ">
-                <div class="card">
-                <form class="box">
-                        <h1>انشاء حساب </h1>
-                        <p class="text-muted"> اهلا بك في متجرنا لإنشاء حساب قم بإدخال بياناتك</p>
-                         <input type="text" name="" placeholder="اسم المستخدم " required>
-                         <input type="password" name="" placeholder="كلمة المرور " required>
-                         <input type="email" name="" placeholder="بريدك الالكتروني " required>
 
+<body>
+    <br><br><br><br><br><br>
+<style>
+    ul.social-network {
+    list-style: none;
+    display: inline;
+    margin-left: 0 !important;
+    padding: 0
+}
+
+ul.social-network li {
+    display: inline;
+    margin: 0 5px
+}
+
+.social-network a.icoFacebook:hover {
+    background-color: #3B5998
+}
+
+.social-network a.icoTwitter:hover {
+    background-color: #33ccff
+}
+
+.social-network a.icoGoogle:hover {
+    background-color: #BD3518
+}
+.social-network a.icoGmail:hover {
+    background-color: #922a13
+}
+.social-network a.icoFacebook:hover i,
+.social-network a.icoTwitter:hover i,
+.social-network a.icoGmail:hover i,
+.social-network a.icoGoogle:hover i {
+    color: black
+}
+
+a.socialIcon:hover,
+.socialHoverClass {
+    color: #44BCDD
+}
+
+.social-circle li a {
+    display: inline-block;
+    position: relative;
+    margin: 0 auto 0 auto;
+    border-radius: 50%;
+    text-align: center;
+    width: 50px;
+    height: 50px;
+    font-size: 20px
+}
+
+.social-circle li i {
+    margin: 0;
+    line-height: 50px;
+    text-align: center
+}
+
+.social-circle li a:hover i,
+.triggeredHover {
+    transform: rotate(360deg);
+    transition: all 0.2s
+}
+
+.social-circle i {
+    color: black;
+    transition: all 0.8s;
+    transition: all 0.8s
+}
+.card button[type="submit"]{
+    border-radius: 24px;
+    cursor: pointer;
+    text-align: center;
+
+
+
+}
+
+.card input, button[type="submit"],label {
+    border: 0;
+    background: none;
+    display: block;
+    margin: 10px ;
+    outline: none;
+    color: black;
+    border-radius: 24px;
+    transition: 0.25s;
+    width: 400px;
+    height: 50px;
+}
+.card input,button[type="submit"]{    border: 2px solid #2ecc71;}
+.cadr button[type="submit"]:hover {
+    background: #2ecc71
+}
+
+</style>
+     <main>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4 "></div>
+        <div class="col-md-4 ">
+            <div class="card" style="border-radius: 24px; padding:10px" >
+                <form class="" action="" method="POST">
+                  <h2 class="text-center" style="color: #000;"> انشاء حساب </h2>
+                  <p class="text-center"> اهلا بك في متجرنا لإنشاء حساب قم بإدخال بياناتك</p>
+                  <div class="form-group text-right">
+                        <label class="text-right" for="uname">اسم المستخدم </label>
+                        <input type="text" class="form-control text-right " id="uname" placeholder="اسم المستخدم" name="uname" required>
+                    </div>
+                    <div class="form-group text-right">
+                        <label class="text-right" for="pass">كلمة المرور  </label>
+                        <input type="password" class="form-control text-right" id="pass" placeholder="كلمة المرور " name="pass" required>
+                    </div>
+                    <div class="form-group text-right">
+                        <label class="text-right" for="email">بريدك الالكتروني   </label>
+                        <input type="email" class="form-control text-right" id="email" placeholder="بريدك الالكتروني  " name="email" required>
+                    </div>
+<br>
+                    <button type="submit" class="btn  btn-primary"  id="submit">التسجيل الان </button>
+                    <p class="text-muted text-center"> او لتسجيل الدخول بحسابات اخرى </p>
+                     <ul class="social-network social-circle navbar-nav" style="text-align:center;padding-left:27%">
                          
-                         <input type="submit" name="" value="التسجيل الان " href="#">
-                         <div class="col-md-12 col-sm-12 col-xs-12 ">
-                         <p class="text-muted"> او لتسجيل الدخول بحسابات اخرى </p>
-                         <ul class="social-network social-circle">
-                                <li><a href="#" class="icoFacebook" title="Facebook"><i class="ion-social-facebook-outline"></i></a></li>
-                                <li><a href="#" class="icoTwitter" title="Twitter"><i class="ion-social-twitter-outline"></i></a></li>
-                                <li><a href="#" class="icoGoogle" title="Google +"><i class="ion-social-google-outline"></i></a></li>
-                            </ul><br>
-                        <a class="forgot text-muted" href="./login"> او للدخول الى حساب موجود س </a>
-
-                        </div>
+                         <li><a href="#" class="icoFacebook" title="Facebook"><i class="ion-social-facebook-outline"></i></a></li>
+                         <li><a href="#" class="icoTwitter" title="Twitter"><i class="ion-social-twitter-outline"></i></a></li>
+                         <li><a href="#" class="icoGoogle" title="Google +"><i class="ion-social-google-outline"></i></a></li>
+                     </ul><br>
+                 <a href="./login"> <p class="text-muted text-center">او للدخول الى حساب موجود  </p></a>
                     </form>
                 </div>
             </div>

72ad6498a90fd35e6843bcfa5af46c6a82d7daf7
HaifaaNabeel <hauof53@gmail.com>
2020-10-26 22:42:08 +0300

    modify about us page

--- app/views/aboutus.php
+++ app/views/aboutus.php
@@ -1,39 +1,190 @@
 <?php include'header.php';?>
  </br></br>
+ 
+    <style>
+        /* Make the image fully responsive */
+        .jumbotron {
+            background-image: url("app/assets/img/about.gif");
 
-<section class="mt-3">
-    <div class="container-fluid">
-        <div class="row">
-                        <div class="col-2"></div>
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: 100% 120%;
+            height: 500px;
+        }
 
-            <div class="col-8">
-<img width="100%" src="app/assets/img/aboutus.jpg">
-    </div>
-            <div class="col-2"></div>
+        .jumbotron h1 {
+            color: #F27523;
+            margin-top: 90px;
+
+        }
+
+
+        .owl-carousel .owl-dots.disabled,
+        .owl-carousel .owl-nav.disabled {
+            display: block;
+            background: transparent;
+        }
+
+        .owl-theme .owl-nav .owl-prev {
+            color: #F27523;
+            margin: 5px;
+            background: transparent;
+            display: inline-block;
+        }
+
+        .owl-theme .owl-nav .owl-next {
+            color: #F27523;
+            margin: 5px;
+            background: transparent;
+            display: inline-block;
+        }
+    </style>
+</head>
+
+<body>
+
+
+    <section class="mt-3">
+        <div class="jumbotron">
+
+            <div class="row">
+                <div class="col-2"></div>
+
+                <div class="col-8 text-center">
+
+                </div>
+
+            </div>
+
+
         </div>
-    </div>
 
-</section>
-<section class="mt-3">
-    <div class="container-fluid">
-        <div class="row">
-                        <div class="col-2"></div>
 
-            <div class="col-8">
-                <p class="text-center" style="font-size: 20px;">
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel justo sed odio pellentesque posuere. Donec non accumsan justo. In a felis sit amet enim rhoncus posuere. Praesent interdum libero vel purus interdum bibendum. Pellentesque at erat nec nulla varius ullamcorper. Ut vel lectus ut sem scelerisque commodo. In ornare nunc tincidunt mollis bibendum. Aliquam justo nulla, scelerisque vel lobortis in, vehicula quis tellus. Nam vitae mauris eu ante tempor luctus quis a odio. Aenean vel turpis et neque scelerisque varius ac sit amet lectus. Morbi tempus, arcu in vulputate congue, odio elit feugiat justo, eget posuere lacus sapien id justo. Aliquam ac aliquam nisi.
 
-Nulla laoreet, sem sit amet gravida porta, nisi nisl ornare mauris, id ultrices nisl metus ut nisl. Etiam eu fermentum ante. Sed maximus luctus nisi, quis sodales lacus. Donec posuere risus sed enim sagittis, a sagittis nulla ornare. Pellentesque malesuada pulvinar dui in tempus. Integer nec pretium risus, a rutrum diam. Aenean sagittis quis ante quis pulvinar. Nunc quis luctus nunc. Donec non dui ultricies, mattis risus sed, tempor neque. 
-                    
-                </p>
-    </div>
-            <div class="col-2"></div>
+    </section>
+
+    <section class="mt-3">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2"></div>
+
+                <div class="col-md-8 text-center">
+                    <h1 style="color:#F27523;">عن المتجر</h1>
+                </div>
+
+            </div>
+            <div class="row">
+                <br>
+                <br>
+
+            </div>
+            <div class="row">
+
+                <div class="col-md-3 text-center ">
+
+                    <img class="img-fluid mx-auto d-block" src="app/assets/img/our-story.png" alt=""
+                        style="margin-top: 100px;">
+                </div>
+
+
+                <div class="col-md-8 ">
+
+                    <p class=" text-justify" style="font-size: 20px; color: #482C8E;">
+                        السوق المفتوح "السوق المفتوح" هو منصة إعلانات مبوبة تقدم خدماتها الإعلانية والتسويقية لجميع
+                        المستخدمين والزائرين في دول الشرق الأوسط، مع أكثر من 2 مليار مشاهدة شهرياً للإعلانات الموجودة
+                        عليها، تقوم بدورها بربط البائعين والمشترين بشكل فعلي؛ لتمكينهم من البيع والشراء أو الحصول على
+                        خدمة أو حتى وظيفة. يتكون فريق العمل في السوق المفتوح من أكثر من 160 موظف يتعاملون مع أكثر من 45
+                        مليون فرد وشركة، ممّن يستخدمون منصتنا لبيع الكثير من السلع والمنتجات بقيمة تزيد عن 25 مليار
+                        دولار سنوياً، وذلك إلى جانب قيمة عروض الوظائف الشاغرة والخدمات التي يتم تداولها عبر المنصة. نحن
+                        في السوق المفتوح نعمل من أجل أن تصبح عمليات البيع والشراء أسهل وأسرع من الأسلوب المعتاد؛ حيث تم
+                        تصميم المنصة لتكون آمنة ومتاحة لجميع دون استثناء، سواء كان المستخدم يمثّل نفسه كـ فرد أو شركة.
+                        منذ تأسيس السوق المفتوح عام 2008 وتسجيله عام 2012، ساهم في خدمة جميع العملاء بمساعدتهم في إتمام
+                        عمليات البيع والشراء والإعلان أو البحث عن خدمة أو وظيفة شاغرة، وأصبح بعد ذلك الخيار الأول
+                        للمستخدمين العرب في منطقة الشرق الأوسط وشمال إفريقيا عبر الإنترنت؛ حيث أننا نقدم خدماتنا في 19
+                        دولة، هي: الأردن، المملكة العربية السعودية، الإمارات العربية المتحدة، الكويت، العراق، سلطنة
+                        عُمان، مصر، البحرين، سوريا، لبنان، ليبيا، السودان، اليمن، قطر، فلسطين، الجزائر، المغرب،
+                        موريتانيا وتونس. نسعى في شركة السوق المفتوح جاهدين لتوفير بيئة إعلانية آمنة وموثوقة عبر
+                        الإنترنت، هدفها إيصال البائعين بالمشترين والعكس أيضاً، بشكل مباشر دون الحاجة إلى وجود وسيط أو
+                        دفع عمولة من خلال القوائم والتصنيفات الموجودة والتي يزيد عددها عن 120، مثل: المركبات، السيارات،
+                        العقارات، الإلكترونيات، ألعاب الفيديو، الهواتف المحمولة، الأثاث، الملابس والأزياء، الكتب
+                        والمجلات ومختلف أنواع الخدمات والقطاعات كذلك. رؤيتنا تتجلّى رؤيتنا في السوق المفتوح بتمكين
+                        الأفراد والشركات من تحقيق الربح من خلال خلق فرص اقتصادية جيدة بمردودها، وتحقيق الرغبات وتلبية
+                        الاحتياجات وتطوير المجتمعات. كيف نعمل؟ يمكن للمستخدمين تحميل تطبيقنا على الـ iOS والآندرويد
+                        للحصول على أفضل تجربة، أو استخدام موقعنا الإلكتروني للبيع والشراء والتقديم للحصول على وظيفة ما
+                        أو تقديم خدمة.
+
+
+
+
+
+
+                    </p>
+
+                </div>
+
+            </div>
         </div>
-    </div>
 
-</section>
- </br></br>
+    </section>
+    <br>
+    <br><br>
 
+    <section class="points">
+        <h1 class=""> علامات تجاريه</h1>
+        <svg id="mesvg" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1400 197.72">
+            <path class="cls-1" d="M0,262.71S322.94-46.71,686.47,108.59,1182.94,245.06,1400,118V262.71Z"
+                transform="translate(0 -64.98)" /></svg>
+    </section>
+    <section class="points2">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1 class="text-center" style="color:#F27523;"> علامات تجاريه</h1>
+                    </br></br>
+                </div>
+                <div class="img-gallery  owl-carousel owl-theme">
+                    <div class="col-sm-6 col-md-3 p-1">
+                        <div class="">
+                            <img class="img-fluid"
+                                src="app/assets/img/Download-Lenovo-Logo-PNG-Transparent-Image-420x186.png">
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-3 p-1">
+                        <div class="">
+                            <img class="img-fluid" src="app/assets/img/Huawei-Symbol.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-3 p-1">
+                        <div class="">
+                            <img class="img-fluid" src="app/assets/img/Sony-Logo-1961%E2%80%931962.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-3 p-1">
+                        <div class="">
+                            <img class="img-fluid" src="app/assets/img/samsung_logo_PNG16.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-3 p-1">
+                        <div class="">
+                            <img class="img-fluid" src="app/assets/img/samsung_logo_PNG16.png">
+                        </div>
+                    </div>
+                </div>
+                </br></br></br></br></br></br>
+            </div>
+        </div>
+    </section>
+
+    <section class="mt-3">
+
+
+    </section>
+
+</body>
+
+</html>
 
 
 

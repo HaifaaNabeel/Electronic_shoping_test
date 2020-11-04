22cd225ed719242b481a453a4ee19c5c6b2098a7
HaifaaNabeel <hauof53@gmail.com>
2020-11-05 00:57:37 +0300

    update of product details without slider

--- app/views/product_details.php
+++ app/views/product_details.php
@@ -12,7 +12,7 @@
  <body>
 
 		<!-- ggggggggggggggggggggggggggggg -->
-		<div class="container ">
+		<div class="container sh_col" dir="rtl" >
 	        <div class="heading-section">
 	            <h2>تفاصيل المنتج</h2>
 			</div>
@@ -25,77 +25,59 @@
 				  
                   
 							echo'
-							<div class="row mb-4">
-								<div class="col-md-2">
-							  <a href="#"><img src="" class="img-thumbnail p-0 border-0" /></a>
-							</div>
-								<div class="col-md-6">
-									<div class="input-group input-group-lg">
-										
-										
-										
-									</div>
-								</div>
-								<div class="col-md-1">
-									
-								</div>
-								<div class="col-md-1">
-									
-								</div>
-								<div class="col-md-2">
-									
-										</div>
-									</div>
-									<br>
-							<div class="row mt-4">
-								<div class="col-md-6 col-sm-6">
-									<div class="exzoom" id="exzoom"><br>
-										<!-- Images -->
-										<div class="exzoom_img_box" style="background:white">
-										  <ul class="exzoom_img_ul">
-										  <li><img src="'.$rows->product_main_image.'"/></li>'?>
-										  <?php
-										   $branch_img=explode(',',$rows->product_branch_images);
-										   for($i=0; $i<sizeof($branch_img)-2; $i++)
-										   echo 
-											'<li><img src="'.$branch_img[$i].'"/></li>
-										   	
-											'?>
-											
-										
-											
-										 <?php
-										 echo' </ul>
-										</div><hr>
-										<!-- Thumbnail Nav-->
-										<div class="exzoom_nav"></div>
-									   
-									  </div>
-									 
+
 							
-								</div>
-								<div class="col-md-6 col-sm-6">
-									<div class="product-dtl">
-										<div class="product-info">
-											<div class="product-name"> '.$rows->product_name.'</div>
+        <div class="row">
+            <div class="col-md-6 sh_img">
+				<img src="'.$rows->product_main_image.'" width="100%" id="productImg">
+                <br>
+                <br>
+                <div class="small-img-row">
+                <div class="small-img-col">
+                        <img src="'.$rows->product_main_image.'" width="100%" class="small-img">
+                    </div>'?>
+				<?php
+				 $branch_img=explode(',',$rows->product_branch_images);
+				 for($i=0; $i<sizeof($branch_img)-2; $i++)
+				 echo 
+				 ' <div class="small-img-col">
+                        <img src="'.$branch_img[$i].'" width="100%" class="small-img">
+                    </div>'?>
+                    
+					<?php
+					echo'    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h1>'.$rows->product_name.'</h1>
+                <h4>'.$rows->product_price.'</h4>
+                
+                <br>
+                <p>'.$rows->product_details.'</p>
+				<br>
+                <div class="product-count mb-4">
+                <h4>الكمية</h4>'
+                ;?>
 											
-											<div class="product-price-discount"><span>'.$rows->product_price.'</span><span class="line-through">$29.00</span></div>
-										</div>
-										<p>'.$rows->product_details.'</p>
-										
-										<div class="product-count">
-											<label for="size">Quantity</label>
-											<form action="#" class="display-flex">
-												<div class="qtyminus">-</div>
-												<input type="text" name="quantity" value="1" class="qty">
-												<div class="qtyplus ">+</div>
-											</form><br>
-											<!--<a href="cart?id=" class="round-black-btn">Add to Cart</a>-->
-											';?>
-           <!-- <button class="btn product_btn m-1 round-black-btn" onclick="favorite()" name="cart"><span class="ion-ios-cart-outline"></span></button>
-                                            <button class="btn product_btn m-1 round-black-btn" onclick="favorite()" name="favorite"><span class="ion-android-favorite-outline"></span></button>
-                                            <button class="btn product_btn m-1 round-black-btn" onclick="filter()"  name="filter"><span class="ion-ios-color-filter-outline"></span></button>-->
-                    <input id="id<?php echo $rows->Product_id; ?>"  name="product_id" value="<?php echo $rows->Product_id; ?>" hidden="hidden">
+				<div class="handle-counter">
+				<button
+					onclick="this.parentNode.querySelector(&#39;input[type=number]&#39;).stepDown()"
+					class="counter-minus btn"><span
+						class="ion-android-remove"></span></button>
+<input id="id<?php echo $item->Product_id; ?>"  name="product_id" value="<?php echo $item->Product_id; ?>" hidden="hidden">
+<input id="id<?php echo $item->Product_id; ?>"  name="price" value="<?php echo $item->product_price; ?>" hidden="hidden">
+<input id="user<?php echo $_GLOBALS['U']; ?>"  name="user" value="<?php echo $_GLOBALS['U']; ?>" hidden="hidden">
+				<input class="quantity text-center" min="1" name="qty"
+					value="<?php  echo $item->product_main_image;?>" type="number">
+				<button
+					onclick="this.parentNode.querySelector(&#39;input[type=number]&#39;).stepUp()"
+					class="counter-plus btn "><span
+						class="ion-android-add"></span></button>
+
+                        <br>
+                        <br>
+                        <br>
+                        <input id="id<?php echo $rows->Product_id; ?>"  name="product_id" value="<?php echo $rows->Product_id; ?>" hidden="hidden">
                             <input id="qty"  name="qty" value="1" hidden="hidden">
                             <input id="user<?php echo $_GLOBALS['U']; ?>"  name="user" value="<?php echo $_GLOBALS['U']; ?>" hidden="hidden">
                         <button class="btn product_btn m-1" id="cart<?php echo $rows->Product_id; ?>" onclick="cart<?php echo $rows->Product_id;?>()"><span class="ion-ios-cart-outline"></span></button>
@@ -107,11 +89,15 @@
                      
                         <button class=" btn product_btn m-1" id="filter<?php echo $rows->Product_id; ?>" onclick="filter<?php echo $rows->Product_id; ?>()" name="filter"><span class="ion-ios-color-filter-outline"></span></button>
                          <button class=" dcart btn product_btn m-1" id="dfilter<?php echo $rows->Product_id; ?>" onclick="dfilter<?php echo $rows->Product_id; ?>()" name="dfilter"><span class="ion-ios-color-filter-outline"></span></button>
-                       
-										</div>
-									</div>
-								</div>
-							</div>
+			</div>
+            </div>
+        </div>
+        </div>
+<hr>
+
+</div>
+
+
  <script>
         
 function filter<?php echo $rows->Product_id; ?>(){
@@ -183,34 +169,47 @@ $.post("add/add_cart/deletefromcart",{product_id:$("#id<?php echo $rows->Product
         
     </script>
 <?php }
-							 ?>
+                             ?>
+                             
+
+
+                            
 			
-		</div>
-	</div>
-</div>	
-			<div class="container ">
-	       
-	        <div class="row">
-	        	<div class="col-md-12">
-					<section>
-					<br><br><br><br><br><br><br>
-	</section>
-	</div>
-		</div>
-		</div>
+		
 	
 	
-	<script type="text/javascript">
-	 $(function(){
- 
-	   $("#exzoom").exzoom({
-		 // options here
-	   });
- 
-	 });
-	</script>
+		<script>
+    var productImg=document.getElementById("productImg");
+    var smallImg=document.getElementsByClassName("small-img");
+
+    smallImg[0].onclick=function(){
+        productImg.src = smallImg[0].src;
+    }
+    smallImg[1].onclick=function(){
+        productImg.src = smallImg[1].src;
+        
+    }
+    smallImg[2].onclick=function(){
+        productImg.src = smallImg[2].src;
+        
+    }
+    smallImg[3].onclick=function(){
+        productImg.src = smallImg[3].src;
+        
+    }
+
+</script>
+
+
+<script>
+
+$('small-img').click(function () {
+    $(this).css('border','1px solid black');
+});
+</script>
 	<!-- partial -->
   <script  src="./script.js"></script>
+
 
 </body>
 
